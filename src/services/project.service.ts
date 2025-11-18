99cf3700a2b65a39560b5fc93a0a5b2c4a2b2166
Andy <andy@linkbot.sg>
2025-11-18 13:50:34 +0700

    feat: add project service

---
+++ src/services/project.service.ts
@@ -0,0 +1,15 @@
+export class ProjectService {
+  async createProject() {}
+
+  async getProjects() {}
+
+  async getProjectById() {}
+
+  async updateProject() {}
+
+  async deleteProject() {}
+
+  async getProjectStats() {}
+}
+
+export const projectService = new ProjectService();
